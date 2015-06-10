5a64283a7425ed18993f984d7d03ffd916b1f9b9
Martin Lantzsch <martin@linux-doku.de>
2015-06-11 00:33:09 +0200

    implemented basic charting library to visualize the crawled data

---
+++ DWD.Charting/Charting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+
+namespace DWD.Charting {
+    public class Charting {
+        public PlotModel Model { get; set; }
+
+        public Charting(int x, int y) {
+            Model = new PlotModel();
+            Model.PlotType = PlotType.XY;
+        }
+
+        public void AddData(Dictionary<DateTime, decimal> data) {
+            var line = new LineSeries();
+
+            foreach (var set in data) {
+                line.Points.Add(new DataPoint(set.Key.ToFileTimeUtc(), (double)set.Value));
+            }
+
+            Model.Series.Add(line);
+        }
+
+        public void SaveToPng(string fileName) {
+            using (var stream = File.Create(fileName)) {
+                var pngExporter = new PngExporter();
+                pngExporter.Export(Model, stream);
+            }
+        }
+    }
+}
--- DWD.Console/Program.cs
+++ DWD.Console/Program.cs
@@ -1,15 +1,21 @@
-﻿using DWD.Crawler.Provider;
+﻿using System.Linq;
+using DWD.Crawler.Provider;
 
 namespace DWD.Console {
     class Program {
         static void Main(string[] args) {
             var temperatureProvider = new AirTemperatureProvider();
+            var charting = new Charting.Charting(10000, 100);
+            charting.AddData(temperatureProvider.GetByStationName("Reimlingen").ToDictionary(x => x.MeasuredAt, x => x.Temperature));
+            charting.SaveToPng("chart.png");
 
             foreach (var temperature in temperatureProvider.GetByStationName("Reimlingen")) {
                 System.Console.WriteLine("{0}: {1} °C", temperature.Station.Name, temperature.Temperature);
             }
 
+
             System.Console.ReadLine();
+
         }
     }
 }
