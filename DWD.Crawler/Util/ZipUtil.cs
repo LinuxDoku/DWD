2303ed7966f07650a23c16bb2cc2f4ccc09c012b
Martin Lantzsch <martin@linux-doku.de>
2015-06-15 23:19:00 +0200

    fixed ZipUtil on Mono (Mono Bug #30686)

--- DWD.Crawler/Util/ZipUtil.cs
+++ DWD.Crawler/Util/ZipUtil.cs
@@ -6,10 +6,12 @@ using System.Linq;
 namespace DWD.Crawler.Util {
     public static class ZipUtil {
         public static Stream Unzip(Stream zipStream, Func<ZipArchiveEntry, bool> selector) {
+			var streamCopy = new MemoryStream();
+			zipStream.CopyTo (streamCopy);
             var unzipped = new MemoryStream();
             var writer = new StreamWriter(unzipped);
 
-            using (var archive = new ZipArchive(zipStream, ZipArchiveMode.Read)) {
+			using (var archive = new ZipArchive(streamCopy, ZipArchiveMode.Read)) {
                 var entry = archive.Entries.First(selector);
                 writer.Write(new StreamReader(entry.Open()).ReadToEnd());
             }
