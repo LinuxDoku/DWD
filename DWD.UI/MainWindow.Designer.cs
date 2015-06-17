9e6bc2d86f02157fa54ade9ef9ab44dd632a5d55
Martin Lantzsch <martin@linux-doku.de>
2015-06-17 22:21:56 +0200

    very basic and unstable ui to play around with the data

---
+++ DWD.UI/MainWindow.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace DWD.UI {
+    partial class MainWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtStationName = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtStationName
+            // 
+            this.txtStationName.Location = new System.Drawing.Point(13, 13);
+            this.txtStationName.Name = "txtStationName";
+            this.txtStationName.Size = new System.Drawing.Size(254, 20);
+            this.txtStationName.TabIndex = 0;
+            this.txtStationName.Text = "Reimlingen";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(273, 12);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 381);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.txtStationName);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStationName;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ DWD.UI/MainWindow.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+using DWD.Crawler.Provider;
+using OxyPlot.WindowsForms;
+
+namespace DWD.UI {
+    public partial class MainWindow : Form {
+        protected AirTemperatureProvider AirTemperatureProvider { get; set; }
+        protected PlotView Plot { get; set; }
+
+        public MainWindow() {
+            InitializeComponent();
+
+            AirTemperatureProvider = new AirTemperatureProvider();
+            Plot = new PlotView {
+                Dock = DockStyle.Fill
+            };
+
+            UpdatePlot();
+            Controls.Add(Plot);
+        }
+
+        protected Charting.Charting LoadTemperature(string stationName) {
+            var temperatures = AirTemperatureProvider.GetByStationName(stationName);
+
+            var charting = new Charting.Charting();
+            charting.AddData(temperatures.ToDictionary(x => x.MeasuredAt, x => x.Temperature));
+
+            return charting;
+        }
+
+        protected void UpdatePlot() {
+            Plot.Model = LoadTemperature(txtStationName.Text).Model;
+        }
+
+        private void btnUpdate_Click(object sender, System.EventArgs e) {
+            UpdatePlot();
+        }
+    }
+}
